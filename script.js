10e1bad0f6ba754022bc667f12a9fdd5ac31b3dd
Susan Marsh <susanlmarsh@users.noreply.github.com>
2020-10-28 13:39:28 +0000

    first working version

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-const first = ['Love', 'Happiness', 'Success', 'Faith', 'Joy', 'Health', 'Financial Freedom'];
-const second = ['passion', 'actions', 'dreams', 'choices', 'desires', 'achievements', 'motivation'];
-const third = ['strong', 'kind', 'wise', 'faithful', 'generous', 'passionate', 'a dreamer'];
-const motivationalMessage = `${word1} is not something ready made, it comes from your own ${word2}.  Be ${word3}`;
+let first = ['Love', 'Happiness', 'Success', 'Faith', 'Joy', 'Health', 'Financial Freedom'];
+let second = ['passion', 'actions', 'dreams', 'choices', 'desires', 'achievements', 'motivation'];
+let third = ['strong', 'kind', 'wise', 'faithful', 'generous', 'passionate', 'a dreamer'];
+let word1, word2, word3, motivationalMessage;
+
+
+function generateRandomWords(arr){
+    return arr[Math.floor(Math.random() * arr.length)];
+    
+}
+
+
+word1 = generateRandomWords(first);
+word2 = generateRandomWords(second);
+word3 = generateRandomWords(third);
+motivationalMessage = `${word1} is not something ready made, it comes from your own ${word2}.  Be ${word3}`;
+console.log(motivationalMessage);
