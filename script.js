83fd0294ae24d5126a29501b7c2924f8be343ecb
Susan Marsh <susanlmarsh@users.noreply.github.com>
2020-10-28 13:16:47 +0000

    variables declared

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 const first = ['Love', 'Happiness', 'Success', 'Faith', 'Joy', 'Health', 'Financial Freedom'];
+const second = ['passion', 'actions', 'dreams', 'choices', 'desires', 'achievements', 'motivation'];
+const third = ['strong', 'kind', 'wise', 'faithful', 'generous', 'passionate', 'a dreamer'];
+const motivationalMessage = `${word1} is not something ready made, it comes from your own ${word2}.  Be ${word3}`;
